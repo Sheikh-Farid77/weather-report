c6516c1c02adeff5941f9cab0698e0aef2a4ee96
Sheikh Farid <mdfaridsagor101@gmail.com>
2025-06-02 12:38:49 +0600

    written full jsx and create useWeather custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-
+import Header from "./components/header/Header";
+import WeatherBoard from "./components/weather/WeatherBoard";
 
 function App() {
- 
-
   return (
-    <>
-    <h1>hello world</h1>
-    
-    </>
-  )
+    <div className="bg-body font-[Roboto] text-light bg-[url('../assets/body-bg.png')] bg-no-repeat bg-cover h-screen grid place-items-center">
+      <Header />
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
